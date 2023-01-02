73da87a714cbdf94b2c60bde71f492922315b4f3
He Lin <helinchooi@gmail.com>
2023-01-02 15:31:55 +0800

    Generate file without calling /

--- index.js
+++ index.js
@@ -8,32 +8,33 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  fs.readFile("sample.prisma", (err, data) => {
-    if (err) throw err;
-
-    console.log(data.toString());
-    const lineArr = data.toString().split(/\r?\n/);
-    let result = "";
-    lineArr.forEach(line => {
-      if (line.startsWith("model ")) {
-        result = result.concat("\n")
-        let tableName = line.replace("model ", "").replace("{", "")
-        result = result.concat(tableName, "\n")
-      } else if (isAttributeLine(line)) {
-        const arr = line.trim().split(/\s+/g);
-        const attribute = arr[0] + ": " + arr[1];
-        console.log('attribute', attribute)
-        result = result.concat("-"+attribute, "\n")
-      }
-    });
-    logger.write(result);
-    res.send(result)
-  });
+  res.send("Hello World")
 })
+fs.readFile("sample.prisma", (err, data) => {
+  if (err) throw err;
+
+  console.log(data.toString());
+  const lineArr = data.toString().split(/\r?\n/);
+  let result = "";
+  lineArr.forEach(line => {
+    if (line.startsWith("model ")) {
+      result = result.concat("\n")
+      let tableName = line.replace("model ", "").replace("{", "")
+      result = result.concat(tableName, "\n")
+    } else if (isAttributeLine(line)) {
+      const arr = line.trim().split(/\s+/g);
+      const type = arr[1].endsWith("?") ? arr[1].substring(0, arr[1].length-1): arr[1]
+      const attribute = arr[0] + ": " + type;
+      console.log('attribute', attribute)
+      result = result.concat("-"+attribute, "\n")
+    }
+  });
+  logger.write(result);
+});
 
 const isAttributeLine = (line) => {
   const arr = line.trim().replace(/\s+/g, ' ').split(" ");
-  console.log(arr)
+  // console.log(arr)
   const ret = line.startsWith("  ")
     && !line.startsWith("  @")
     && arr[1] && !arr[1].startsWith("@")
